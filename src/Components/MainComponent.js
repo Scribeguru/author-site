548e5ffbff26d36eb833f676462f52686b268769
Scribeguru <pocomitch@gmail.com>
2022-03-17 17:06:35 -0700

    img links to technologies, color changes, tweaked responsiveness, changed fonts

--- src/Components/MainComponent.js
+++ src/Components/MainComponent.js
@@ -8,6 +8,9 @@ export default function Main() {
 
   useEffect(() => {
     setTimeout(() => toggleCollapse(), 6000);
+    const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
+    const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
+    console.log('viewport width: ', vw, 'viewport height:', vh);
   }, [setOpen])
 
   function toggleCollapse() {
@@ -16,35 +19,41 @@ export default function Main() {
 
   return (
     <>
-      
-        <Row className="hi-there-container">
-          <Col className="hi-there">
-            <span className="hG">H</span>
-            <span className="iG">i </span>
-            <span className="tG">t</span>
-            <span className="hGtwo">h</span>
-            <span className="eG">e</span>
-            <span className="rG">r</span>
-            <span className="eGtwo">e</span>
-            <span className="bangG">!</span>
-          </Col>
-        </Row>
-        <Row className="my-name-is-container">
-          <Col className="my-name-is">
-            My name's <span className="mitch">Mitch</span>.
-          </Col>
-          <Collapse isOpen={isOpen}>
-            <Col className="opening-msg text-center">
-              I'm a California-based software developer with experience in React, Node.js, Express.js, and MongoDB.
+      <Row className="hi-there-container">
+        <Col className="hi-there">
+          <span className="hG">H</span>
+          <span className="iG">i </span>
+          <span className="tG">t</span>
+          <span className="hGtwo">h</span>
+          <span className="eG">e</span>
+          <span className="rG">r</span>
+          <span className="eGtwo">e </span>
+          <span className="bangG">!</span>
+        </Col>
+      </Row>
+      <Row className="my-name-is-container text-center">
+        <Col className="my-name-is text-nowrap">
+          My name's <span className="mitch">Mitch</span>.
+        </Col>
+        <Collapse isOpen={isOpen}>
+          <Col className="opening-msg text-center">
+            <p>I'm a California-based software developer with experience in React, Node.js, Express.js, and MongoDB.</p>
+            <Col>
+              <a href="https://www.mongodb.com/" target="blank"><img className="hvr-backward mern-l img-fluid" src="/images/mongosymbol.png" alt="mongo icon" /></a>
             </Col>
-          </Collapse>
-        </Row>
-        <Row>
-          <Col className="downArrow bounce">
-            <i class="fa fa-angle-down fa-3x"></i>
+            <Col>
+              <a href="https://expressjs.com/" target="blank"><img className="hvr-forward mern-r img-fluid" src="/images/expresssymbol.png" alt="express icon" /></a>
           </Col>
-        </Row>
-      
+          <Col>
+            <a href="https://reactjs.org/" target="blank"><img className="hvr-backward mern-l img-fluid" src="/images/reactsymbol.png" alt="react icon" /></a>
+        </Col>
+        <Col>
+          <a href="https://nodejs.org/en/" target="blank"><img className="hvr-forward mern-r img-fluid" src="/images/nodesymbol.png" alt="node icon" /></a>
+      </Col>
+    </Col>
+        </Collapse >
+      </Row >
+    <div className="extra" />
     </>
   );
 }
