72e6dda95dc2f25d1845d0fffa8fe85f4dff22ec
Scribeguru <pocomitch@gmail.com>
2022-03-22 22:55:59 -0700

    more responsive

--- src/Components/MainComponent.js
+++ src/Components/MainComponent.js
@@ -7,7 +7,7 @@ export default function Main() {
   const [isOpen, setOpen] = useState(false);
 
   useEffect(() => {
-    setTimeout(() => toggleCollapse(), 5350);
+    setTimeout(() => toggleCollapse(), 5500);
     const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
     const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
     console.log('viewport width: ', vw, 'viewport height:', vh);
@@ -21,13 +21,13 @@ export default function Main() {
     <>
       <Row className="nav-container container-fluid pt-4">
         <Col xs="12" className="social">
-          <a href="https://codepen.io/mitchellpoco/pens/public" target="_blank"><img src="/images/codepen.png" height="45px" width="205px" /></a>
+          <a className="social-h" href="https://codepen.io/mitchellpoco/pens/public" target="_blank"><img src="/images/codepen.png" height="45px" width="205px" /></a>
         </Col>
-        <Col xs="12">
-          <a href="https://github.com/Scribeguru" target="_blank"><i className="social fa fa-github fa-3x" /></a>
+        <Col xs="12"  className="social">
+          <a className="social-h" href="https://github.com/Scribeguru" target="_blank"><i className="social fa fa-github fa-3x" /></a>
         </Col>
-        <Col xs="12">
-          <a href="https://www.linkedin.com/in/mitchellpoco/" target="_blank"><i className="social fa fa-linkedin fa-3x" /></a>
+        <Col xs="12"  className="social">
+          <a className="social-h" href="https://www.linkedin.com/in/mitchellpoco/" target="_blank"><i className="social fa fa-linkedin fa-3x" /></a>
         </Col>
       </Row>
       <Row className="hi-there-container">
@@ -43,33 +43,25 @@ export default function Main() {
         </Col>
       </Row>
       <Row>
-        <Col className="my-name-is text-nowrap">
+        <Col xs="12" className="my-name-is text-nowrap">
           My name's <span className="mitch">Mitch</span>.
         </Col>
-      </Row>
-      <Row className="img-intro">
-        <Collapse isOpen={isOpen}>
-          <Col className="img-bar">
-            <Col>
+        <Col className="img-intro">
+          <Collapse isOpen={isOpen}>
+            <Col className="img-bar text-center">
               <img className="on-bike img-fluid mb-3" src="/images/onbike1.PNG" alt="me on my motorbike" />
             </Col>
-          </Col>
-        </Collapse >
-      </Row>
-      <Row>
+          </Collapse >
+        </Col>
         <Col className="opening-msg">
-          <p>I'm a California-based software developer with experience in React, Node.js, Express.js, and MongoDB.</p>
+          <p className="text-intro"><em>
+            I'm a California-based software developer with experience in React, Node.js, Express.js, and MongoDB.
+          </em></p>
           <Col>
-            <a href="https://www.mongodb.com/" target="blank"><img className="hvr-backward mern-l img-fluid" src="/images/mongosymbol.png" alt="mongo icon" /></a>
-          </Col>
-          <Col>
-            <a href="https://expressjs.com/" target="blank"><img className="hvr-forward mern-r img-fluid" src="/images/expresssymbol.png" alt="express icon" /></a>
-          </Col>
-          <Col>
-            <a href="https://reactjs.org/" target="blank"><img className="hvr-backward mern-l img-fluid" src="/images/reactsymbol.png" alt="react icon" /></a>
-          </Col>
-          <Col>
-            <a href="https://nodejs.org/en/" target="blank"><img className="hvr-forward mern-r mb-4 img-fluid" src="/images/nodesymbol.png" alt="node icon" /></a>
+            <a href="https://www.mongodb.com/" target="blank"><img className="mongos hvr-forward mern img-fluid" src="/images/mongosymbol.png" alt="mongo icon" /></a>
+            <a href="https://expressjs.com/" target="blank"><img className="expresss hvr-forward mern img-fluid" src="/images/expresssymbol.png" alt="express icon" /></a>
+            <a href="https://reactjs.org/" target="blank"><img className="reacts hvr-forward mern img-fluid" src="/images/reactsymbol.png" alt="react icon" /></a>
+            <a href="https://nodejs.org/en/" target="blank"><img className="nodes hvr-forward mern img-fluid" src="/images/nodesymbol.png" alt="node icon" /></a>
           </Col>
         </Col>
       </Row>
